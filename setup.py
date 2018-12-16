1df4cd38bab9c0e7a38796f212ff886a0ee50376
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-12-15 23:05:23 -0800

    Add license to setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     author_email="raymond.ehlers@cern.ch",
 
     url="https://github.com/raymondEhlers/pachyderm",
+    license="BSD 3-Clause",
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
