a698a490d53a344d3065f1e4b4dad97a5dfa7036
Raymond Ehlers <raymond.ehlers@gmail.com>
2019-02-22 12:58:46 -0500

    Clean comments, descriptions, and spacing

--- setup.py
+++ setup.py
@@ -26,26 +26,26 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="pachyderm",
-    version=get_version(),
+    name = "pachyderm",
+    version = get_version(),
 
-    description="Physics Analysis Core for Heavy-Ions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    description = "Physics Analysis Core for Heavy-Ions",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
 
-    author="Raymond Ehlers",
-    author_email="raymond.ehlers@cern.ch",
+    author = "Raymond Ehlers",
+    author_email = "raymond.ehlers@cern.ch",
 
-    url="https://github.com/raymondEhlers/pachyderm",
-    license="BSD 3-Clause",
+    url = "https://github.com/raymondEhlers/pachyderm",
+    license = "BSD 3-Clause",
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
+    classifiers = [
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
@@ -57,9 +57,9 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='HEP ALICE',
+    keywords = 'HEP ALICE',
 
-    packages=find_packages(exclude=(".git", "tests")),
+    packages = find_packages(exclude=(".git", "tests")),
 
     # Rename scripts to the desired executable names
     # See: https://stackoverflow.com/a/8506532
@@ -69,14 +69,13 @@ setup(
     },
 
     # This is usually the minimal set of the required packages.
-    # Packages should be installed via pip -r requirements.txt !
-    install_requires=[
+    install_requires = [
         "dataclasses;python_version<'3.7'",
         "ruamel.yaml",
         "numpy",
         "uproot",
         # Depends on ROOT, but that can't be installed through pip.
-        # The dependence is only implicit, so it can actually be installed with it,
+        # The dependence is only implicit, so it can actually be installed without it,
         # but some functionality depends on it being available.
         #"ROOT"
     ],
