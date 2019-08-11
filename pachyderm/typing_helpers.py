9894c3f526712f6eb72df1c0d454444992d58dcb
Raymond Ehlers <raymond.ehlers@gmail.com>
2019-08-11 16:56:55 -0400

    Prevent ROOT from intercepting args
    Otherwise, import the type helpers would prevent command line args from
    being parsed properly

--- pachyderm/typing_helpers.py
+++ pachyderm/typing_helpers.py
@@ -9,11 +9,21 @@ from typing import Any, Union
 
 try:
     import ROOT
+    # Tell ROOT to ignore command line options so args are passed to python
+    # NOTE: Must be immediately after import ROOT and it must be called the first time ROOT is imported!
+    #       We do this here (even though it is unrelated to typing helpers) because it is the most common
+    #       first import which requires ROOT. So by putting it here, it should (hopefully) cover all executables.
+    #       However, this means that it needs to be imported before some pachyderm modules.
+    ROOT.PyConfig.IgnoreCommandLineOptions = True
+
     Hist = Union[ROOT.TH1, ROOT.THnBase]
     Axis = ROOT.TAxis
+    Canvas = ROOT.TCanvas
     TFile = ROOT.TFile
 except ImportError:
     # It doesn't like the possibility of redefining this, so we need to tell ``mypy`` to ignore it.
     Hist = Any  # type: ignore
     Axis = Any
+    Canvas = Any
     TFile = Any
+
